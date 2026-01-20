d5ec926fa58967f2d4f8e6fc7688672a62772118
Nithin30121 <2500030121@kluniversity.in>
2026-01-20 10:31:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div class="card">
+    <div class="profile-img">
+        <img src="https://avatars.githubusercontent.com/u/64242199?v=4" alt="Nithin Kondeti"/>
+    </div>
+    <h2>Nithin Kondeti</h2>
+    <p class="role">Web Developer</p>
+    <p class="desc">
+        Passionate about design, clean code, and building cool things on the web.
+    </p>
+    </div>
+
+  )
+}
+
+export default App
